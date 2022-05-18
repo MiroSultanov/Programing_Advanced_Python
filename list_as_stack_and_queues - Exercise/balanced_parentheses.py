e04041cf6c1a7b9cecb94dc89c57f1377ed36ba6
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-05-18 06:15:09 +0300

    Update balanced_parentheses.py

--- list_as_stack_and_queues - Exercise/balanced_parentheses.py
+++ list_as_stack_and_queues - Exercise/balanced_parentheses.py
@@ -1,3 +1,11 @@
+# You will be given a sequence consisting of parentheses. Your job is to determine whether the expression is balanced. A sequence of parentheses is balanced
+# if every opening parenthesis has a corresponding closing parenthesis that occurs after the former. There will be no interval symbols between the parentheses. 
+# You will be given three types of parentheses: (), {}, and [].
+# {[()]} - Parentheses are balanced.
+# (){}[] - Parentheses are balanced.
+# {[(])} - Parentheses are NOT balanced.
+
+
 expression = input()
 opening_brackets = []
 balanced = True
