d8216773a5b37243bde2c478cfc52a2b9c0ca9fa
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-05-20 06:30:57 +0300

    Update record_unique_names.py

--- tuples_and_sets - Lab/record_unique_names.py
+++ tuples_and_sets - Lab/record_unique_names.py
@@ -1,3 +1,7 @@
+# Write a program, which will take a list of names and print only the unique names in the list.
+# The order in which we print the result does not matter.
+
+
 number_of_names = int(input())
 names_set = set()
 
