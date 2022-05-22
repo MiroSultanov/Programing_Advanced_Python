566ad4dc4c5b2a7ec2e8e339b32838c68dcf8dcf
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-05-22 13:11:25 +0300

    Update longest_intersection.py

--- tuples_and_sets - Exercise/longest_intersection.py
+++ tuples_and_sets - Exercise/longest_intersection.py
@@ -1,3 +1,9 @@
+# Write a program that finds the longest intersection. You will be given a number N. On each of the next N lines you will be given two ranges in the 
+# format: "{first_start},{first_end}-{second_start},{second_end}". You should find the intersection of these two ranges. The start and end numbers in the ranges are 
+# inclusive. Finally, you should find the longest intersection of all N intersections, print the numbers that are included and its length in the 
+# format: "Longest intersection is [{longest_intersection_numbers}] with length {length_longest_intersection}"
+
+
 number_of_intersection = int(input())
 best_intersection = set()
 
