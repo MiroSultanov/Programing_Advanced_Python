269768843ab7d59ea6baf383d056701c9daeef2e
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-05-26 06:17:53 +0300

    Update sum_matrix_elements.py

--- multidimensional_lists - Lab/sum_matrix_elements.py
+++ multidimensional_lists - Lab/sum_matrix_elements.py
@@ -1,3 +1,10 @@
+# Write a program that reads a matrix from the console and prints:
+# •	The sum of all numbers in the matrix
+# •	The matrix itself
+# On the first line, you will receive the matrix sizes in the format "{rows}, {columns}". On the next rows, you will get elements for each column separated by a 
+# comma and a space ", ". 
+
+
 n, m = [int(x) for x in input().split(', ')]
 matrix = []
 matrix_sum = 0
