52b87ad06f6e582f603d8f55c950eb4e2257b13b
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-05-31 06:33:55 +0300

    Update operate.py

--- functions advanced - Lab/operate.py
+++ functions advanced - Lab/operate.py
@@ -1,3 +1,8 @@
+# Write a function called operate that receives an operator ("+", "-", "*" or "/") as first argument and multiple numbers (integers) as additional arguments (*args).
+# The function should return the result of the operator applied to all the numbers. For more clarification, see the examples below. 
+# Submit only your function in the Judge system.
+
+
 def operate(operator, *args):
     result = args[0]
 
