2ad11f3e431ee710c6f83ce6b00841331fac46ee
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-05-31 06:34:24 +0300

    Update person_info.py

--- functions advanced - Lab/person_info.py
+++ functions advanced - Lab/person_info.py
@@ -1,3 +1,7 @@
+# Write a function called get_info that receives a name, an age, and a town and returns a string in the format: 
+# "This is {name} from {town} and he is {age} years old". Use dictionary unpacking when testing your function. Submit only the function in the judge system.
+
+
 def get_info(name, age, town):
     return f"This is {name} from {town} and he is {age} years old"
 
