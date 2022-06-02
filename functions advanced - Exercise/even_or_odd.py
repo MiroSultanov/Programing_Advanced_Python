4dca379520a1dc9c1856b17a9051ab6dec018067
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-06-02 06:45:27 +0300

    Update even_or_odd.py

--- functions advanced - Exercise/even_or_odd.py
+++ functions advanced - Exercise/even_or_odd.py
@@ -1,3 +1,6 @@
+# Create a function called even_odd() that can receive a different quantity of numbers and a command at the end. The command can be "even" or "odd". 
+# Filter the numbers depending on the command and return them in a list.
+
 def even_odd(*args):
     filter_command = args[-1]
     parity = 0 if filter_command == 'even' else 1
