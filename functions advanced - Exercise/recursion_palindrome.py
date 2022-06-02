3ddb76a763710b875c15150b406a427d5f8e9368
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-06-02 06:49:47 +0300

    Update recursion_palindrome.py

--- functions advanced - Exercise/recursion_palindrome.py
+++ functions advanced - Exercise/recursion_palindrome.py
@@ -1,3 +1,7 @@
+# Write a recursive function called palindrome() that will receive a word and an index (always 0). Implement the function, so it returns "{word} is a palindrome"
+# if the word is a palindrome and "{word} is not a palindrome" if the word is not a palindrome using recursion. 
+
+
 def palindrome(word, index):
     if index >= len(word) // 2:
         return f"{word} is a palindrome"
