08dbab4f828c14719c062c219e8c79ac048b55a2
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-06-07 05:36:18 +0300

    Update file_opener.py

--- file_handling - Lab/2.file_opener/file_opener.py
+++ file_handling - Lab/2.file_opener/file_opener.py
@@ -1,3 +1,13 @@
+# You are given a file called numbers.txt with the following content:
+# 1
+# 2
+# 3
+# 4
+# 5
+
+# Create a program that reads the numbers from the file. Print on the console the sum of those numbers.
+
+
 file_path = './text.txt' # File found
 # file_path = './text2.txt'  # File not found
 
