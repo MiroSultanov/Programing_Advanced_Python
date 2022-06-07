f5496931add26e78b8f8e5044cbb439017153c8c
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-06-07 05:35:27 +0300

    Update file_reader.py

--- file_handling - Lab/1.file_reader/file_reader.py
+++ file_handling - Lab/1.file_reader/file_reader.py
@@ -1,3 +1,12 @@
+# You are given a file called text.txt with the following text:
+    
+# This is some random line
+# This is the second line
+# And this is the third one
+
+# Create a program that opens the file. If the file is found, print 'File found'. If the file is not found, print 'File not found'.
+
+
 file = open('./numbers.txt', 'r')
 the_sum = 0
 
