a1a1eb1bc109c6bf4c8fd86ee4071876ab2ca4cc
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-06-07 05:37:39 +0300

    Update file_delete.py

--- file_handling - Lab/4.file_delete/file_delete.py
+++ file_handling - Lab/4.file_delete/file_delete.py
@@ -1,3 +1,5 @@
+# Create a program that deletes the file you created in the previous task. If you try to delete the file multiple times, print the message: 'File already deleted!'.
+
 import os
 try:
     os.remove('my_first_file.txt')
