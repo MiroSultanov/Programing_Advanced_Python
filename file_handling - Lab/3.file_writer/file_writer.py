85d974a6c54d8d6f7b72fc787affeedffdb5954b
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-06-07 05:37:09 +0300

    Update file_writer.py

--- file_handling - Lab/3.file_writer/file_writer.py
+++ file_handling - Lab/3.file_writer/file_writer.py
@@ -1,2 +1,4 @@
+# Create a program that creates a file called my_first_file.txt. In that file, write a single line with the content: 'I just created my first file!'
+
 created_file = open('my_first_file.txt', 'w')
 created_file.write('I just created my first file!')
