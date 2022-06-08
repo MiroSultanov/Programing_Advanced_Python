891c229a63cda93cd4b848e67e04aaa182462070
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-06-08 06:12:34 +0300

    Update even_lines.py

--- file_handling - Exercise/1.even_lines/even_lines.py
+++ file_handling - Exercise/1.even_lines/even_lines.py
@@ -1,3 +1,12 @@
+# Write a program that reads a text file and prints on the console its even lines. Line numbers start from 0. Before you print the result, 
+# replace {"-", ",", ".", "!", "?"} with "@" and reverse the order of the words.
+
+# Input text:
+# -I was quick to judge him, but it wasn't his fault.
+# -Is this some kind of joke?! Is it?
+# -Quick, hide here. It is safer.
+
+
 symbol_for_replace = ["-", ",", ".", "!", "?"]
 with open('./text.txt','r') as file:
     for index, line in enumerate(file):
