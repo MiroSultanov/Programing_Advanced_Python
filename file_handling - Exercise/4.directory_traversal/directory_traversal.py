085601b8021f075126771224fb70a54d5e514f4f
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-06-08 06:15:54 +0300

    Update directory_traversal.py

--- file_handling - Exercise/4.directory_traversal/directory_traversal.py
+++ file_handling - Exercise/4.directory_traversal/directory_traversal.py
@@ -1,3 +1,7 @@
+# Write a program that traverses a given directory for all files. Search through the first level of the directory only and write information about each found file 
+# in report.txt. The files should be grouped by their extension. Extensions should be ordered by name alphabetically. The files with extensions should also be 
+# sorted by name. report.txt should be saved in the chosen directory.
+
 from os import listdir
 from os.path import isdir, join
 
