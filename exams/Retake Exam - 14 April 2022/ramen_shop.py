fcf570c9021790c9f9d02f67967e6715999c2557
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-06-16 06:16:48 +0300

    Update ramen_shop.py

--- exams/Retake Exam - 14 April 2022/ramen_shop.py
+++ exams/Retake Exam - 14 April 2022/ramen_shop.py
@@ -1,3 +1,12 @@
+# You will be given two sequences of integers representing bowls of ramen and customers. Your task is to find out if you can serve all the customers.
+# Start by taking the last bowl of ramen and the first customer. Try to serve every customer with ramen until we have no more ramen or customers left:
+# •	Each time the value of the ramen is equal to the value of the customer, remove them both and continue with the next bowl of ramen and the next customer.
+# •	Each time the value of the ramen is bigger than the value of the customer, decrease the value of that ramen with the value of that customer and remove the 
+# customer. Then try to match the same bowl of ramen (which has been decreased) with the next customer.
+# •	Each time the customer's value is bigger than the value of the ramen bowl, decrease the value of the customer with the value of the ramen bowl and remove the bowl. Then try to match the same customer (which has been decreased) with the next bowl of ramen.
+# Look at the examples provided for a better understanding of the problem.
+
+
 from collections import deque
 
 bowls_of_ramen = deque([int(x) for x in input().split(", ")])
