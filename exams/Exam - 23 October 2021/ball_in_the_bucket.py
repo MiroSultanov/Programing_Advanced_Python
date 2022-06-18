8d064e2457c44df43c3dd57e2283cf69bf14c3ab
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-06-18 09:20:05 +0300

    Update ball_in_the_bucket.py

--- exams/Exam - 23 October 2021/ball_in_the_bucket.py
+++ exams/Exam - 23 October 2021/ball_in_the_bucket.py
@@ -1,3 +1,21 @@
+# You are at the funfair to play different games and test your skills. Now you are playing ball in the bucket game.
+# You will be given a matrix with 6 rows and 6 columns representing the board. On the board, there will be points (integers) and buckets marked with the 
+# letter "B". Rules of the game:
+# •	You can throw a ball only 3 times.
+# •	When you hit a bucket (position marked with 'B'), you score the sum of the points in the same column.
+# •	You can hit a bucket only once. If you hit the same bucket again, you do not score any points. 
+# •	If you hit outside a bucket (hit a number on the board) or outside the board, you do not score any points. 
+# After the board state, you are going to receive the information for every throw on a separate line. The coordinates’ information of a hit will be in 
+# the format: "({row}, {column})".
+# Depending on how many points you have collected, you win one of the following:
+#  Football	             100 to 199 points
+# Teddy Bear	            200 to 299 points
+# Lego Construction Set	300 and more points
+
+# Your job is to keep track of the scored points and to check if you won a prize. 
+# For more clarifications, see the examples below.
+
+
 def get_cell(cell: tuple, matrix: list) -> str:
     row, col = cell
     return matrix[row][col]
