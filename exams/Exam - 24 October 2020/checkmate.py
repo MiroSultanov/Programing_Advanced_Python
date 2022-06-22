12b775b5245867728ccec0ff7200d5cb016c5948
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-06-22 10:09:55 +0300

    Update checkmate.py

--- exams/Exam - 24 October 2020/checkmate.py
+++ exams/Exam - 24 October 2020/checkmate.py
@@ -1,3 +1,22 @@
+# You will be given a chess board (8x8). On the board there will be 3 types of symbols:
+# •	"." – empty square
+# •	"Q" – a queen
+# •	"K" – the king
+# Your job is to find which queens can capture the king and print them. The moves that the queen can do is to move diagonally, horizontally and vertically 
+# (basically all the moves that all the other figures can do except from the knight). Beware that there might be queens that stand in the way of other queens 
+# and can stop them from capturing the king. For more clarification see the examples.
+# Input
+# •	8 lines – the state of the board (each square separated by single space)
+# Output
+# •	The positions of the queens that can capture the king as lists
+# •	If the king cannot be captured, print: "The king is safe!"
+# •	The order of output does not matter
+# Constrains
+# •	There will always be exactly 8 lines
+# •	There will always be exactly one King
+# •	Only the 3 symbols described above will be present in the input
+
+
 def is_inside(row, col, matrix_size):
     return 0 <= row < matrix_size and 0 <= col < matrix_size
 
