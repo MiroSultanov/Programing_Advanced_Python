a032429d8e2abf5b445a6583d83a2d9040eec2fc
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-06-23 08:30:13 +0300

    Update taxi_express.py

--- exams/Retake Exam - 19 August 2020/taxi_express.py
+++ exams/Retake Exam - 19 August 2020/taxi_express.py
@@ -1,3 +1,19 @@
+# You will receive a list of the cutomers (numbers seperated by comma and space ", ") on the first line and list of your taxis (numbers seperated by comma and space ", ").
+# Each number from the customer list represents how much time it takes to drive the customer to his/her destination.
+# Each number from the taxis list represents how much time they can drive, before they need to refill their tanks.
+# Keep track of the total time passed to drive all the customers to their destinations (values of all customers).
+# Each time you tend customers you should put the first customer in the last taxi until there are no customers left.
+# •	If the taxi can drive the customer to his/her destination, he does and you must add the time passed to drive the customer to his/her destination (the value of the
+# current customer) to the total time. Remove both the customer and the taxi.
+# •	If the taxi cannot drive the customer to his/her destination, leave the customer at the beginning of the queue and remove the taxi. 
+# At the end if you have successfully driven all the customers to their destinations, print 
+# All customers were driven to their destinations
+# Total time: {total_time} minutes
+# Otherwise, if you ran out of taxis and there are still some customers left print
+# Not all customers were driven to their destinations
+# Customers left: {left_customers joined by ", "}
+
+
 from collections import deque
 
 customers = deque([int(x) for x in input().split(", ")])
